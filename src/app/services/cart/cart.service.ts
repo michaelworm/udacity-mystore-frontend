3d6d6deb3ff31f654a90ba3fdfa0d39e7c9db610
Michael Worm <michael.worm@essquare.de>
2021-08-25 13:47:39 +0200

    Optimize ui

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -8,7 +8,15 @@ export class CartService {
   cart: Array<CartProduct> = []
 
   getCart (): Array<CartProduct> {
-    return this.cart
+    return [{
+      id: 1,
+      name: "Book",
+      price: 9.99,
+      url: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      description: "You can read it!",
+      quantity: 3
+    }]
+    //return this.cart
   }
   addProductToCart (quantity: number, product: Product): void {
     this.cart.push({
