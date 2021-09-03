f01a40d6532d6261417e68f6b2036a20c465db58
Michael Worm <michael.worm@essquare.de>
2021-09-03 13:48:18 +0200

    Add cart register name validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -8,9 +8,16 @@ import {Customer, EmptyCustomer} from "../../interfaces/cart"
 })
 export class RegisterComponent {
   customer: Customer = EmptyCustomer
+  isNameValid: boolean = true
   @Output() registeredCustomer: EventEmitter<Customer> = new EventEmitter<Customer>()
 
   registerCustomerSubmit (): void {
     this.registeredCustomer.emit(this.customer)
+  }
+
+  checkValidName(value: Event) {
+    const regex = /^[A-Za-z0-9]+$/
+
+    this.isNameValid = regex.test(value as unknown as string)
   }
 }
